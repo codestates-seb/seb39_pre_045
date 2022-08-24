e8e834d9999ab47337363d16f25cf4c34137e9fc
y0ungg <m0_23@naver.com>
2022-08-24 23:50:09 +0900

    feat: sidebar component 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Nav from './Components/Nav';
+import SideBar from './Components/SideBar';
 function App() {
   return (
     <div className="App">
       <Nav />
+      <SideBar />
     </div>
   );
 }
