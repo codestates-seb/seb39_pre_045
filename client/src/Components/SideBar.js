7e82f616d6349e0ebdb503a0e7f575557dcba94c
민영 <104131962+y0ungg@users.noreply.github.com>
2022-08-25 15:41:48 +0900

    fix: Login컴포넌트, SideBar 컴포넌트 css수정 (#16)
    * feat: Login, Main 컴포넌트 생성, Route 적용
    
    * fix: Login컴포넌트, SideBar 컴포넌트 css수정

--- client/src/Components/SideBar.js
+++ client/src/Components/SideBar.js
@@ -2,11 +2,31 @@ import styled from 'styled-components';
 // import { useState } from 'react';
 
 const Container = styled.nav`
-  width: 164px;
+  width: 230px;
+  padding: 0;
+  height: 100vh;
+  border-right: 0.3px solid rgba(77, 77, 77, 0.45);
+  ul {
+    margin-top: 70px;
+  }
+  @media only screen and (max-width: 767px) {
+    display: none;
+  }
 `;
+
 const Li = styled.li`
   padding: 8px 6px 8px 0;
   list-style: none;
+  color: ${(props) => props.color || '#383938'};
+  margin-bottom: ${(props) => props.margin || '0'};
+  font-size: ${(props) => props.size || '14px'};
+  span {
+    font-size: 14px;
+    margin-right: 5px;
+  }
+  &:hover {
+    cursor: ${(props) => props.cursor || 'pointer'};
+  }
 `;
 
 const SideBar = () => {
@@ -14,9 +34,13 @@ const SideBar = () => {
   return (
     <Container>
       <ul>
-        <Li>Home</Li>
-        <Li>Public</Li>
-        <Li>Questions</Li>
+        <Li margin="10px">Home</Li>
+        <Li cursor="default" size="11px" color="#656765">
+          PUBLIC
+        </Li>
+        <Li>
+          <span className="material-icons">language</span>Questions
+        </Li>
         {/* {isLogin ? <li>Users</li> : null} */}
       </ul>
     </Container>
