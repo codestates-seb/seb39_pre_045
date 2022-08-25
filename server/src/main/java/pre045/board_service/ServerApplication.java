ff4bf87212f6943facc34000a2950e5134571350
jphwany <pjh5762@naver.com>
2022-08-25 22:52:40 +0900

    feat: server init

---
+++ server/src/main/java/pre045/board_service/ServerApplication.java
@@ -0,0 +1,12 @@
+package pre045.board_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
