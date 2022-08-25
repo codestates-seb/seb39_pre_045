117774e2894c1187950281225b424cf2d4b49500
y0ungg <m0_23@naver.com>
2022-08-25 11:13:20 +0900

    feat: Login, Main 컴포넌트 생성, Route 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Nav from './Components/Nav';
 import SideBar from './Components/SideBar';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
+
 function App() {
   return (
     <div className="App">
       <Nav />
-      <SideBar />
+      <div className="container">
+        <SideBar />
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
