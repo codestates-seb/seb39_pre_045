c7b96c2a0d9a96b000dcb12496c9b6fd475153d3
Yunjin Lee <98820643+sweesweett@users.noreply.github.com>
2022-08-25 11:41:04 +0900

    Feat/nav (#14)
    * fix:초기 설정 오류 수정
    
    * feat: Nav component 생성
    
    * feat: Nav component css 1차 수정

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -2,44 +2,66 @@ import styled from 'styled-components';
 const NavHeader = styled.header`
   width: 100%;
   height: 50px;
-  background-color: aqua;
+  background-color: #e1ecf4;
+  box-sizing: border-box;
   .headerContainer {
     display: flex;
-    width: 100%;
-    padding: 10px;
+    padding: 10px 20px;
     max-width: 1200px;
     margin: auto;
     justify-content: space-between;
     align-items: center;
   }
   .logo {
+    content: '';
     flex: 1 1 10%;
-    background-color: blue;
+    background-image: url('../image/stckoverflow.png');
+    background-position: 0 -500px;
+    /* width: 50px;
+    height: 50px; */
   }
   .searchHeader {
     flex: 1 1 70%;
-    background-color: red;
     display: flex;
-    border: 2px solid black;
+    border: 1px solid #babfc4;
     border-radius: 5px;
+    background-color: white;
     span {
       flex: 1 1 5%;
       text-align: center;
       line-height: 30px;
+      color: #babfc4;
+      font-size: 24px;
     }
     input {
       flex: 1 1 95%;
       padding: 5px;
+      border: none;
+      ::placeholder {
+        color: #babfc4;
+      }
     }
   }
   .logInOut {
     flex: 1 1 20%;
-    text-align: right;
+    margin-left: 10px;
     span {
-      padding: 5px;
+      display: inline-block;
+      padding: 6px;
       border-radius: 5px;
       background-color: azure;
       margin-right: 10px;
+      font-size: 14px;
+      border: 1px solid hsl(205, 41%, 63%);
+      cursor: pointer;
+      :last-child {
+        color: white;
+
+        background-color: #6bbbf7;
+        :hover {
+          background-color: #0074cc;
+        }
+      }
     }
   }
 `;
@@ -48,14 +70,14 @@ const Nav = () => {
   return (
     <NavHeader className="header">
       <div className="headerContainer">
-        <div className="logo">여기로고</div>
+        <div className="logo"></div>
         <div className="searchHeader">
           <span className="material-icons md-18">search</span>
-          <input type="text" maxLength={30} />
+          <input type="text" maxLength={30} placeholder="Search..." />
         </div>
         <div className="logInOut">
-          <span>login</span>
-          <span>sign-up</span>
+          <span>Log in</span>
+          <span>Sign up</span>
         </div>
       </div>
     </NavHeader>
