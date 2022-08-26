832363f2785bed0266a838b9882dc20dff4be5bf
sweesweett <sweesweet@naver.com>
2022-08-26 09:36:43 +0900

    overwriten된거 수정

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import link from '../image/stackoverflow.png';
 const NavHeader = styled.header`
   width: 100%;
   height: 50px;
-  background-color: #e1ecf4;
+  background-color: #f8f9f9;
+  color: #232629;
   box-sizing: border-box;
+  position: fixed;
+  z-index: 5;
+  box-shadow: 0px 2px 5px #b0bec5;
+  .topColored {
+    width: 100%;
+    height: 3px;
+    background-color: orange;
+  }
   .headerContainer {
     display: flex;
     padding: 10px 20px;
@@ -14,18 +25,24 @@ const NavHeader = styled.header`
   }
   .logo {
     content: '';
-    flex: 1 1 10%;
-    background-image: url('../image/stckoverflow.png');
+    flex: 1 1 15%;
+    background-image: url(${link});
+    background-repeat: no-repeat;
     background-position: 0 -500px;
+    width: 146px;
+    height: 30px;
+    cursor: pointer;
+
     /* width: 50px;
     height: 50px; */
   }
   .searchHeader {
-    flex: 1 1 70%;
+    flex: 1 1 65%;
     display: flex;
     border: 1px solid #babfc4;
     border-radius: 5px;
     background-color: white;
+
     span {
       flex: 1 1 5%;
       text-align: center;
@@ -37,6 +54,7 @@ const NavHeader = styled.header`
       flex: 1 1 95%;
       padding: 5px;
       border: none;
+      outline: none;
       ::placeholder {
         color: #babfc4;
       }
@@ -47,17 +65,17 @@ const NavHeader = styled.header`
     margin-left: 10px;
     span {
       display: inline-block;
-      padding: 6px;
-      border-radius: 5px;
-      background-color: azure;
+      padding: 5px 7px;
+      border-radius: 3px;
+      background-color: #e1ecf4;
       margin-right: 10px;
       font-size: 14px;
+      color: #39739d;
       border: 1px solid hsl(205, 41%, 63%);
       cursor: pointer;
-      :last-child {
+      &.signupBtn {
         color: white;
-
-        background-color: #6bbbf7;
+        background-color: #0a95ff;
         :hover {
           background-color: #0074cc;
         }
@@ -69,15 +87,22 @@ const NavHeader = styled.header`
 const Nav = () => {
   return (
     <NavHeader className="header">
+      <div className="topColored"></div>
       <div className="headerContainer">
-        <div className="logo"></div>
+        <div className="logo" url={link}>
+          {/* <img src={link} alt="" /> */}
+        </div>
         <div className="searchHeader">
           <span className="material-icons md-18">search</span>
           <input type="text" maxLength={30} placeholder="Search..." />
         </div>
         <div className="logInOut">
-          <span>Log in</span>
-          <span>Sign up</span>
+          <Link to={'/login'}>
+            <span className="loginBtn">Log in</span>
+          </Link>
+          <Link to={'/signup'}>
+            <span className="signupBtn">Sign up</span>
+          </Link>
         </div>
       </div>
     </NavHeader>
