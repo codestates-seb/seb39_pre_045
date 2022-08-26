15a6462a2a0f76f68ded702debdfb7648575f66e
sweesweett <sweesweet@naver.com>
2022-08-27 00:36:54 +0900

    충돌방지 커밋

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -40,6 +40,7 @@ const NavHeader = styled.header`
     min-width: 146px;
     height: 30px;
     cursor: pointer;
+
     /* width: 50px;
     height: 50px; */
   }
@@ -86,6 +87,7 @@ const NavHeader = styled.header`
         color: white;
         background-color: #0a95ff;
         margin-right: 0;
+        box-shadow: inset 0 1px 0 0 rgba(255, 255, 255, 0.4);
         :hover {
           background-color: #0074cc;
         }
@@ -137,6 +139,7 @@ const NavHeader = styled.header`
 const Nav = () => {
   const [isblock, setIsblock] = useState(false);
   const searchVal = useRef();
+
   // const [searchVal, setSearchVal] = useState('');
   const showBlock = () => {
     setIsblock(!isblock);
@@ -152,9 +155,9 @@ const Nav = () => {
   return (
     <NavHeader className="header" display={`${isblock}`}>
       <div className="headerContainer">
-        <div className="logo" url={link}>
-          {/* <img src={link} alt="" /> */}
-        </div>
+        <Link to={'/'}>
+          <div className="logo" url={link}></div>
+        </Link>
 
         <div className="searchHeader">
           <span className="material-icons md-18">search</span>
