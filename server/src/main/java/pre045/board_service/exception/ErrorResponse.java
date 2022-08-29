ce5900894c711d7498410259602d6bc5f15b8ae5
romcanrom <baros2san@gmail.com>
2022-08-29 21:48:21 +0900

    fix: answer impl ver2. exception handling and URI refactoring

---
+++ server/src/main/java/pre045/board_service/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package pre045.board_service.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String message;
+
+    private ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(ExceptionCode exceptionCode) {
+        return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
+    }
+
+
+}
