5075f65b4a1150ce691e5ba83d43d5f24c63affe
Yunjin Lee <98820643+sweesweett@users.noreply.github.com>
2022-08-30 14:02:23 +0900

    'fix:DetailQuestion page/Answer,Comment,Question,LikeOrDisLike component' (#68)
    * feat:DetailQuestion Page+ 관련된 컴포넌트 생성, 등록, 수정페이지 css
    
    * 충돌방지 commit
    
    * fix: DetailQuestion Page, LikeOrdislike Component
    
    * fix:DetailQuestion page/Answer,Comment,Question,LikeOrDisLike component

--- client/src/Components/Question.js
+++ client/src/Components/Question.js
@@ -1,2 +1,94 @@
-const Question = () => {};
+import LikeOrDislike from '../Components/LikeOrDislike';
+import styled from 'styled-components';
+import MarkdownViewer from '../Components/MarkdownViewer';
+import Comment, { WriteComment } from './Comment';
+import { useState } from 'react';
+
+const QuestionDiv = styled.div`
+  display: flex;
+  max-width: 1000px;
+  width: 100%;
+  height: 100%;
+  .test {
+    width: 100%;
+    margin-left: 30px;
+  }
+  ul.comment {
+    padding: 0;
+    max-width: 1000px;
+  }
+  .addComment {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+`;
+export const InfoBarDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  span {
+    margin: 0 10px;
+    a {
+      text-decoration: none;
+      color: #0c0d0e;
+    }
+  }
+  .userInfo {
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+  }
+`;
+
+const Question = ({ question }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleWrite = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <QuestionDiv className="wrapper">
+      <LikeOrDislike className="rateLike" />
+      <div className="test">
+        <MarkdownViewer />
+        <InfoBarDiv>
+          <div>
+            <span>
+              <a href="/">Edit</a>
+            </span>
+            <span>
+              <a href="/">Delete</a>
+            </span>
+          </div>
+          <div className="userInfo">
+            <span>asked {question.createdAt}</span>
+            <span>{question.author}</span>
+          </div>
+        </InfoBarDiv>
+        <ul className="comment">
+          {question.comments.map((el, index) => (
+            <Comment comment={el} key={index} />
+          ))}
+        </ul>
+        <div>
+          {isOpen === false ? (
+            <button className="addComment" onClick={() => setIsOpen(!isOpen)}>
+              add comment...
+            </button>
+          ) : (
+            <WriteComment>
+              <span>comment</span>
+              <button className="close" onClick={() => setIsOpen(!isOpen)}>
+                x
+              </button>
+              <textarea id="editComment" />
+              <button onClick={handleWrite} className="submitComment">
+                Edit
+              </button>
+            </WriteComment>
+          )}
+        </div>
+      </div>
+    </QuestionDiv>
+  );
+};
 export default Question;
