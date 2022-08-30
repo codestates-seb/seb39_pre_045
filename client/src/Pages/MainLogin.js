5b784167d06fd559c98c2f9e7a7a930a54de47af
sweesweett <sweesweet@naver.com>
2022-08-30 21:51:13 +0900

    fix:page/component Signup, Nav component , MainLogout, MainLogin, SearchResult

--- client/src/Pages/MainLogin.js
+++ client/src/Pages/MainLogin.js
@@ -37,7 +37,7 @@ const MainLogin = () => {
     <MainContainer>
       <div className="pageDesc">
         <h2>Top Questions</h2>
-        <button>Ask Question</button>
+        <button onClick={() => navigate('/write')}>Ask Question</button>
       </div>
 
       <RightAlignBtns>
