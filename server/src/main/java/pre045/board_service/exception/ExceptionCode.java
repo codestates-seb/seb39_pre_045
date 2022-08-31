ec24bca4a62bf63a749b2f7607526993615440ae
Lego79 <kentcandoit89@gmail.com>
2022-08-31 10:10:37 +0900

    fix:Exception code Comment ë¶관련 exception code 추가

--- server/src/main/java/pre045/board_service/exception/ExceptionCode.java
+++ server/src/main/java/pre045/board_service/exception/ExceptionCode.java
@@ -9,6 +9,12 @@ public enum ExceptionCode {
 
     QUESTION_NOT_FOUND(404,"유효한 질문이 아닙니다."),
 
+    COMMENT_CANNOT_EDIT(403, "댓글 작성자만 수정할 수 있습니다."),
+
+    MEMBER_NOT_FOUND(404, "유효한 작성자가 아닙니다."),
+
+    COMMENT_NOT_FOUND(404, "유효한 댓글이 아닙니다."),
+
     ANSWER_CANNOT_DELETE(403, "채택된 답변은 삭제할 수 없습니다."),
     ANSWER_DUPLICATE_ADOPT(403, "답변 채택은 한번만 가능합니다."),
     ANSWER_CANNOT_POST(401, "답변은 회원만 작성할 수 있습니다."),
