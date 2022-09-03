a3c365c85933f9f49506c3f90d25ea6399590147
jphwany <pjh5762@naver.com>
2022-09-04 00:18:47 +0900

    fix&feat

--- server/src/main/java/pre045/board_service/question/dto/QuestionPostDto.java
+++ server/src/main/java/pre045/board_service/question/dto/QuestionPostDto.java
@@ -2,12 +2,14 @@ package pre045.board_service.question.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class QuestionPostDto {
 
     // Post 요청으로 들어오는 값을 이렇게 받는다
