75d582e9c11a50858d32370011f21d9986700838
Lego79 <kentcandoit89@gmail.com>
2022-09-05 01:17:27 +0900

    feat:AnswerVote Refactoring

--- server/src/main/java/pre045/board_service/vote/answer_vote/controller/AnswerVoteController.java
+++ server/src/main/java/pre045/board_service/vote/answer_vote/controller/AnswerVoteController.java
@@ -3,14 +3,8 @@ package pre045.board_service.vote.answer_vote.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pre045.board_service.dto.SingleResponseDto;
-import pre045.board_service.vote.answer_vote.dto.AnswerVoteDto;
-import pre045.board_service.vote.answer_vote.entity.AnswerVote;
-import pre045.board_service.vote.answer_vote.mapper.AnswerVoteMapper;
 import pre045.board_service.vote.answer_vote.service.AnswerVoteService;
 
 @RestController
@@ -19,23 +13,16 @@ import pre045.board_service.vote.answer_vote.service.AnswerVoteService;
 public class AnswerVoteController {
 
     private final AnswerVoteService answerVoteService;
-    private final AnswerVoteMapper mapper;
+
 
     @PostMapping("/up")
-    public ResponseEntity upAnswer(@RequestBody AnswerVoteDto.Post post) {
-        AnswerVote answerVote = mapper.answerVotePostToAnswerVote(post);
-        AnswerVote upVote = answerVoteService.upVote(post.getMemberId(), answerVote);
-        AnswerVoteDto.Response response = mapper.answerVoteToAnswerVoteResponse(upVote);
+    public ResponseEntity upAnswer(@PathVariable("answer-id") Long answerId) {
 
-        return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.CREATED);
+        return new ResponseEntity(new SingleResponseDto<>(answerVoteService.upVote(answerId)), HttpStatus.CREATED);
     }
 
     @PostMapping("/down")
-    public ResponseEntity downAnswer(@RequestBody AnswerVoteDto.Post post) {
-        AnswerVote answerVote = mapper.answerVotePostToAnswerVote(post);
-        AnswerVote downVote = answerVoteService.downVote(post.getMemberId(), answerVote);
-        AnswerVoteDto.Response response = mapper.answerVoteToAnswerVoteResponse(downVote);
-
-        return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.CREATED);
+    public ResponseEntity downAnswer(@PathVariable("answer-id") Long answerId) {
+        return new ResponseEntity(new SingleResponseDto<>(answerVoteService.downVote(answerId)), HttpStatus.CREATED);
     }
 }
