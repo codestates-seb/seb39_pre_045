6a1b25571e2c89006f697e81c20e0e1dcab366df
민영 <104131962+y0ungg@users.noreply.github.com>
2022-09-04 17:33:44 +0900

    fix: 마크다운뷰어, 에디터 데이터  가져오기 완료 (#122)
    * feat: MobileSideBar/stores/로그인 로직구현
    
    * fix: 로그인 로직구현(store에 로그인여부 저장, local storage에 액세스토큰저장, App에 로그인여부 state 연결)
    
    * fix: 질문작성페이지 axios 통신로직에  Headers 설정
    
    * fix: 상세페이지 store생성, 연결/ 페이지라우트params변경
    
    * fix: 마크다운뷰어, 에디터 데이터  가져오기 완료

--- client/src/Components/MarkdownViewer.js
+++ client/src/Components/MarkdownViewer.js
@@ -2,33 +2,19 @@ import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import { Viewer } from '@toast-ui/react-editor';
 import styled from 'styled-components';
 
+const MarkdownViewer = (props) => {
+  const words = `<p>글자</p>`;
+  return (
+    <Div margin={props.margin}>
+      <Viewer initialValue={props.value || words} />
+    </Div>
+  );
+};
+
 const Div = styled.div`
   margin: ${(props) => props.margin || '50px 0'};
   /* height: 500px; */
   /* border: 1px solid red; */
   overflow-y: scroll;
 `;
-
-const MarkdownViewer = (props) => {
-  const words = `
-  ## 뷰어 체크중
-  잘 **나**오나
-  # hi
-  dd\`hi\`dd
-  ddddd
-  please write here
-
-***
-백틱 앞에 백슬래시 추가해야함
-\`\`\`;
-  ㅇㅇㅇㅇ
-  \`\`\`
-  `;
-  return (
-    <Div margin={props.margin}>
-      <Viewer initialValue={words} />
-    </Div>
-  );
-};
-
 export default MarkdownViewer;
