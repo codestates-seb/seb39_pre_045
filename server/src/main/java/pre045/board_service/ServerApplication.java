508e2a7de634ac9338097731b56d77abfe8ce8ed
jphwany <pjh5762@naver.com>
2022-09-08 11:05:10 +0900

    main merge

--- server/src/main/java/pre045/board_service/ServerApplication.java
+++ server/src/main/java/pre045/board_service/ServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+// merge to main
 @EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
